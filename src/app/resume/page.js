8b0ab9b3a54dce081594534c1e5e79e4d1d92625
Dylan Pratt <dylanjohn0506@gmail.com>
2025-12-09 00:44:15 -0700

    fixed the resume page

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Navbar from "@/components/Navbar";
 import Background from "@/components/Background";
