6ab730a68d2b6c94af114dd591297bc4ba4bea99
Dylan Pratt <dylanjohn0506@gmail.com>
2025-12-10 14:22:21 -0700

    Project Modal enchancements

--- data/projectsData.js
+++ data/projectsData.js
@@ -4,36 +4,60 @@ const projects = [
       id: 1,
       title: "Project SwordBreak",
       image: "/images/swordBreak.png",
-      description: "Project Swordbreak is a solo-developed action-adventure game built in Unreal Engine, leveraging C++ and Blueprints for fluid combat, responsive movement, and immersive world interaction. Key technical highlights include a custom-built randomizer system, custom localization system, and an advanced character controller.",
-      link: "https://legendaryepics.itch.io/project-swordbreak"
+      description: "Project Swordbreak is a action-adventure game built in Unreal Engine. Key technical highlights include a custom-built randomizer system, custom localization system, and an advanced character controller.",
+      link: "https://legendaryepics.itch.io/project-swordbreak",
+      technologies: [
+        { name: "Unreal Engine", color: "#ffffffff" },
+        { name: "C++", color: "#0091ffff" },
+        { name: "Blueprints", color: "#0066CC" },
+      ]
     },
     {
       id: 2,
       title: "Neon Knight",
       image: "/images/neonKnight.png",
-      description: "This project was my entry for the 2023 Epic MegaJam—a fast-paced third-person bullet-hell. It features a custom Niagara particle collision system.",
-      link: "https://legendaryepics.itch.io/neon-knight"
+      description: "This project was my entry for the 2023 Epic MegaJam. It is a fast-paced third-person bullet-hell. It features a custom Niagara particle collision system.",
+      link: "https://legendaryepics.itch.io/neon-knight",
+      technologies: [
+        { name: "Unreal Engine", color: "#ffffffff" },
+        { name: "Niagara", color: "#a800f7ff" },
+        { name: "C++", color: "#0091ffff" },
+      ]
     },
     {
       id: 3,
       title: "Music Library App",
       image: "/images/musicLib.png",
-      description: "This is a public-facing web application I built for The Church of Jesus Christ of Latter-day Saints.It features a React frontend with a Node.js middleware layer that interfaces with multiple AWS Lambda functions. Deployed on AWS, it leverages services such as CloudFront, API Gateway, and S3 for scalability and high performance.",
-      link: "https://www.churchofjesuschrist.org/media/music?lang=eng"
+      description: "This is a public-facing web application I built for The Church of Jesus Christ of Latter-day Saints. It features a React frontend with a Node.js middleware layer. Deployed on AWS, it leverages services such as CloudFront, API Gateway, AWS Lambda, and S3 for scalability and high performance.",
+      link: "https://www.churchofjesuschrist.org/media/music?lang=eng",
+      technologies: [
+        { name: "React", color: "#00bef3ff" },
+        { name: "Node.js", color: "#339933" },
+        { name: "AWS Lambda", color: "#FF9900" },
+        { name: "CloudFront", color: "#FF9900" },
+        { name: "API Gateway", color: "#FF9900" },
+        { name: "S3", color: "#FF9900" }
+      ]
     },
     {
       id: 4,
       title: "Project ISO",
       image: "/images/projectIso.png",
       description: "????",
-      link: ""
+      link: "",
+      technologies: []
     },
     {
       id: 5,
       title: "MapList UE5 Plugin",
       image: "/images/unrealcpp.png",
-      description: "An Unreal Engine 5 plugin I created to list all maps in a project for quick navigation. It speeds up development by removing the need to manually search for levels, making workflow more efficient.",
-      link: "https://github.com/dylanjp/UE5-MapList-Plugin"
+      description: "An UE5 plugin I built to list all maps in a project for quick navigation.",
+      link: "https://github.com/dylanjp/UE5-MapList-Plugin",
+      technologies: [
+        { name: "Unreal Engine", color: "#ffffffff" },
+        { name: "Plugin Development", color: "#ffffffff"  },
+        { name: "C++", color: "#0091ffff" },
+      ]
     }
     // Add more project objects here
   ];
